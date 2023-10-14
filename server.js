84d3fb80b8398fdea70bb1a770336d890b29526c
elidallen <mitchellallen2@yahoo.com>
2023-10-14 00:53:44 -0400

    asas

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', './views/pug');
+
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('pug/index.pug')
+  res.render('pug/index')
 });
 
 const PORT = process.env.PORT || 3000;
