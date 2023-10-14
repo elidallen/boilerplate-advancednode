aa2afc051b175481e37034a246d1e7633818d5f6
elidallen <mitchellallen2@yahoo.com>
2023-10-14 00:56:43 -0400

    asdasdasd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 
 app.set('view engine', 'pug');
-
+app.set('views', './views');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
